66c8cd85ca999af9d611d22e8b0171c63506f92d
Apan Qasem <apanqasem@gmail.com>
2015-11-06 10:01:31 -0600

    generalized exp_driver.sh

--- utils/exp_driver.sh
+++ utils/exp_driver.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-#datasets="tai30a tai30b tai40a tai40b tai50a tai50b tai60a tai60b tai80a tai80b tai100a tai100b lipa70a lipa90"
-
-datasets="lipa30" 
+datasets="tai30a tai30b tai40a tai40b tai50a tai50b tai60a tai60b tai80a tai80b tai100a tai100b lipa70a lipa90"
 progs="tabu_dyn"
 
 for prog in $progs; do
